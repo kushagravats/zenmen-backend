f4b8e818b278209e04559291fb6c29856778dadb
Kushagra Vats <44410587+kushagravats@users.noreply.github.com>
2024-09-20 20:42:37 +0530

    Update Notification.js

--- zenmen-frontend/src/components/Notification.js
+++ zenmen-frontend/src/components/Notification.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
-
-const socket = io("http://localhost:5000");
 
 const Notification = () => {
   const [notifications, setNotifications] = useState([]);
+  const socket = io("http://localhost:5000");
 
   useEffect(() => {
     socket.on("notification", (data) => {
@@ -13,16 +12,22 @@ const Notification = () => {
     });
 
     return () => {
-      socket.off("notification");
+      socket.disconnect();
     };
-  }, []);
+  }, [socket]);
 
   return (
     <div>
       <h2>Notifications</h2>
       <ul>
         {notifications.map((notification, index) => (
-          <li key={index}>
+          <li
+            key={index}
+            style={getNotificationStyle(
+              // @ts-ignore
+              notification.type
+            )}
+          >
             {
               // @ts-ignore
               notification.message
@@ -32,6 +37,17 @@ const Notification = () => {
       </ul>
     </div>
   );
+};
+
+const getNotificationStyle = (type) => {
+  switch (type) {
+    case "connection":
+      return { color: "green" };
+    case "disconnection":
+      return { color: "red" };
+    default:
+      return { color: "black" };
+  }
 };
 
 export default Notification;
